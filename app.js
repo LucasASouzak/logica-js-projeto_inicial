725cfb8cde7da45e2a428e939a86d9b8b963a0cc
LUCAS A. SOUZA <lucas.antonio.souza@gmail.com>
2024-06-21 10:15:16 -0300

    //Implementando numero aleatório, teste okay.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 //implementando função para manupular texto no HTML.
 
+let numeroSecreto = 100;
+let segredo = criaNumeroRandom();
+
+
 function exibiTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibiTextoNaTela('h1''Jogo do Número Secreto');
-exibiTextoNaTela('p''Escolha um número entre 1 e 100');
+exibiTextoNaTela('h1','Jogo do Número Secreto');
+exibiTextoNaTela('p','Escolha um número entre 1 e 100');
 
+//Implementando numero aleatório, teste okay.
 function verificarChute() {
-   console.log('Botão acionado');
+   console.log(segredo);
+}
+
+function criaNumeroRandom (){
+    return parseInt(Math.random() * numeroSecreto + 1);
 }
