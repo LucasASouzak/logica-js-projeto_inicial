d04abdb009d9b4c9d7b410cc7fd7146271dec98d
LUCAS A. SOUZA <lucas.antonio.souza@gmail.com>
2024-06-25 10:19:04 -0300

    Correções de loop, e reinicio.

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 //implementando função para manupular texto no HTML.
 
 let segredo = numeroRandom();
-
+let tentativas = 1;
 
 function exibiTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibiTextoNaTela('h1','Jogo do Número Secreto');
-exibiTextoNaTela('p','Escolha um número entre 1 e 10');
-
-//Implementando numero aleatório, teste okay.
-function numeroRandom (){
-    return parseInt(Math.random() * 10 + 1);
+function exibeSaudacao(){
+    exibiTextoNaTela('h1','Jogo do Número Secreto');
+    exibiTextoNaTela('p','Escolha um número entre 1 e 10');
 }
 
-
+exibeSaudacao();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == segredo) {
         exibiTextoNaTela('h1', 'Acertou!');
-            exibiTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`;
+            exibiTextoNaTela('p', mensagemTentativas);
+            document.getElementById('reiniciar').removeAttribute('disabled');
         } else {
             if (chute > segredo) {
                 exibiTextoNaTela('p', 'O número secreto é menor');
@@ -32,6 +32,24 @@ function verificarChute() {
                     exibiTextoNaTela('p', 'O número secreto é maior');
                         exibiTextoNaTela('h1', 'Tente de novo.');
                 }
+                tentativas ++;
+                limpaTela();
         }
 }
 
+function numeroRandom (){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limpaTela(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    segredo = numeroRandom();
+    limpaTela();
+    tentativas = 1;
+    exibeSaudacao();
+    document.getElementById ('reiniciar').setAttribute('disabled',true);
+}
