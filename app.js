db6c4391b955882bafe6d0efc663472b0a286c35
LUCAS A. SOUZA <lucas.antonio.souza@gmail.com>
2024-06-25 13:00:36 -0300

    Implementando acessibilidade, API resposiveVoice criada e configurada no codigo;

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let tentativas = 1;
 function exibiTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibeSaudacao(){
@@ -43,7 +44,7 @@ function numeroRandom (){
     let numerosJaSorteados = listaDeNumerosSorteados.length;
 
     if (numerosJaSorteados == limitador){
-        listaDeNumerosSorteados = []
+        listaDeNumerosSorteados = [];
 
     }
     if (listaDeNumerosSorteados.includes(numeroEscolhido)){
